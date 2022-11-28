834cd73fe73588cba8390ca33ac28cb77f69b4c8
Kenzo Wong <kenzowong86@gmail.com>
2022-11-28 00:07:26 -0500

    AWS lightsail migration, campfinder link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,36 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React from 'react';
+import { motion } from 'framer-motion';
 
 function Home() {
   return (
     <motion.div
-      className="home"
+      className='home'
       initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 1 }}
     >
-      <div className="home d-flex flex-column align-items-center mx-auto container-fluid w-100 h-100">
-        <div className="d-flex flex-row">
-          <div className="row d-flex">
-            <div className="d-flex flex-column">
-              <div className="home-intro">
+      <div className='home d-flex flex-column align-items-center mx-auto container-fluid w-100 h-100'>
+        <div className='d-flex flex-row'>
+          <div className='row d-flex'>
+            <div className='d-flex flex-column'>
+              <div className='home-intro'>
                 <h1>
-                  Hi, I'm <span className="highlight-name">Kenzo Wong</span>
+                  Hi, I'm <span className='highlight-name'>Kenzo Wong</span>
                 </h1>
                 <h1>
-                  I'm a web developer in{" "}
-                  <span className="highlight-city">NYC</span>
+                  I'm a web developer in{' '}
+                  <span className='highlight-city'>NYC</span>
                 </h1>
               </div>
-              <div className="d-flex flex-column justify-content-center align-items-center home-blurb">
-                <p className="highlight">I have some experience using:</p>
+              <div className='d-flex flex-column justify-content-center align-items-center home-blurb'>
+                <p className='highlight'>I have some experience using:</p>
 
-                <div className="d-flex flex-column home-blurb-tools">
+                <div className='d-flex flex-column home-blurb-tools'>
                   <p>HTML, CSS, Javascript, Bootstrap, EJS, React, Redux</p>
                   <p>
                     Express, NodeJS, MongoDB, Mongoose, PostgreSQL, Sequelize
                   </p>
-                  <p>Git, Github, Mapbox, WebSockets</p>
+                  <p>AWS Lightsail, Heroku, Git, Github, Mapbox, WebSockets</p>
                 </div>
                 <div></div>
               </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,66 +1,66 @@
-import React from "react";
-import { Carousel } from "react-bootstrap";
-import { motion } from "framer-motion";
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import { motion } from 'framer-motion';
 
 function Projects() {
   return (
     <motion.div
-      className="projects"
+      className='projects'
       initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 1 }}
     >
-      <div className="campfinder d-flex flex-row">
-        <div className="project-pictures row d-flex justify-content-center">
-          <div className="project-pictures-container col-sm-6">
-            <div className="project-pictures-title">
-              <h3 className="highlight">Campfinder</h3>
+      <div className='campfinder d-flex flex-row'>
+        <div className='project-pictures row d-flex justify-content-center'>
+          <div className='project-pictures-container col-sm-6'>
+            <div className='project-pictures-title'>
+              <h3 className='highlight'>Campfinder</h3>
             </div>
 
-            <div className="carouselDiv mb-2">
-              <Carousel className="carousel" controls={false}>
+            <div className='carouselDiv mb-2'>
+              <Carousel className='carousel' controls={false}>
                 <Carousel.Item>
-                  <div className="d-flex justify-content-center carousel-images-maxed">
+                  <div className='d-flex justify-content-center carousel-images-maxed'>
                     <img
-                      className="d-block carousel-images-fixed"
-                      src={"campfinder1.png"}
-                      alt="First slide"
+                      className='d-block carousel-images-fixed'
+                      src={'campfinder1.png'}
+                      alt='First slide'
                     />
                   </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <div className="d-flex justify-content-center carousel-images-maxed">
+                  <div className='d-flex justify-content-center carousel-images-maxed'>
                     <img
-                      className="d-block carousel-images-fixed"
-                      src={"campfinder2.png"}
-                      alt="First slide"
+                      className='d-block carousel-images-fixed'
+                      src={'campfinder2.png'}
+                      alt='First slide'
                     />
                   </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <div className="d-flex justify-content-center carousel-images-maxed">
+                  <div className='d-flex justify-content-center carousel-images-maxed'>
                     <img
-                      className="d-block carousel-images-fixed"
-                      src={"campfinder3.png"}
-                      alt="First slide"
+                      className='d-block carousel-images-fixed'
+                      src={'campfinder3.png'}
+                      alt='First slide'
                     ></img>
                   </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <div className="d-flex justify-content-center carousel-images-maxed">
+                  <div className='d-flex justify-content-center carousel-images-maxed'>
                     <img
-                      className="d-block carousel-images-fixed"
-                      src={"campfinder4.png"}
-                      alt="First slide"
+                      className='d-block carousel-images-fixed'
+                      src={'campfinder4.png'}
+                      alt='First slide'
                     />
                   </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                  <div className="d-flex justify-content-center carousel-images-maxed">
+                  <div className='d-flex justify-content-center carousel-images-maxed'>
                     <img
-                      className="d-block carousel-images-fixed"
-                      src={"campfinder5.png"}
-                      alt="First slide"
+                      className='d-block carousel-images-fixed'
+                      src={'campfinder5.png'}
+                      alt='First slide'
                     />
                   </div>
                 </Carousel.Item>
@@ -68,13 +68,13 @@ function Projects() {
             </div>
           </div>
 
-          <div className="project-description col-sm-6">
-            <div className="project-description-title">
-              <a href="http://kenzowong-campfinder.herokuapp.com/">Website</a>
+          <div className='project-description col-sm-6'>
+            <div className='project-description-title'>
+              <a href='https://campfinder.kenzowong.net/'>Website</a>
               <span> -- </span>
-              <a href="https://github.com/kenzowong/campfinder">Github repo</a>
+              <a href='https://github.com/kenzowong/campfinder'>Github repo</a>
             </div>
-            <div className="mt-3">
+            <div className='mt-3'>
               <p>
                 Campfinder is an extension of YelpCamp, the capstone project
                 students finish from 'The Web Developer Bootcamp' by Colt
